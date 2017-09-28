23f9e28bcab06ff42ba9aaea4323bdb8a7b26d26
Anton <anton.pasichnyk@gmail.com>
2017-09-28 16:44:40 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const Telegram = require('telegram-node-bot'),
+    PersistentMemoryStorage = require('./adapters/PersistentMemoryStorage'),
+    storage = new PersistentMemoryStorage(
+        `${__dirname}/data/userStorage.json`,
+        `${__dirname}/data/chatStorage.json`
+    ),
+    tg = new Telegram.Telegram('148637856:AAEr6HQo7AKAzGw1Kl_evJas3LkFacErYSI', {
+        workers: 1,
+        storage: storage
+    });
+
+const TodoController = require('./controllers/todo')
+    , OtherwiseController = require('./controllers/otherwise');
+
+const todoCtrl = new TodoController();
+
+tg.router.when(new Telegram.TextCommand('/add', 'addCommand'), todoCtrl)
+    .when(new Telegram.TextCommand('/get', 'getCommand'), todoCtrl)
+    .when(new Telegram.TextCommand('/check', 'checkCommand'), todoCtrl)
+    .when(new Telegram.TextCommand('/rates', 'ratesCommand'), todoCtrl)
+    .otherwise(new OtherwiseController());
+
+function exitHandler(exitCode) {
+    storage.flush();
+    process.exit(exitCode);
+}
+
+process.on('SIGINT', exitHandler.bind(null, 0));
+process.on('uncaughtException', exitHandler.bind(null, 1));
